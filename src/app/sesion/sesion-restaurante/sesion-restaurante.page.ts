9306be653327ea5e7867b8eb8ecd0c4755ed0c58
oriana castellano <orianacastella@gmail.com>
2021-07-26 10:04:30 -0400

    subida ori 9

--- src/app/sesion/sesion-restaurante/sesion-restaurante.page.ts
+++ src/app/sesion/sesion-restaurante/sesion-restaurante.page.ts
@@ -6,7 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sesion-restaurante.page.scss'],
 })
 export class SesionRestaurantePage implements OnInit {
+  lista: string[];
 
+  producto: any=[{
+    titulo: "Hamburguesa",
+    restaurante: "Siesta key",
+    precio: "3$" ,
+    ubicacion: "Av.13A entre calles 72 y 73",
+  },{
+    titulo: "Pizza Personal",
+    restaurante: "Caste Pizza",
+    precio: "1$" ,
+    ubicacion: "Av.Padilla-Torres del Saladillo",},
+    {
+      titulo: "Torta de Chocolate",
+      restaurante: "Gelart",
+      precio: "5$" ,
+      ubicacion: "Urb.Coromoto. Calle 171 con av.43",},
+];
   constructor() { }
 
   ngOnInit() {
