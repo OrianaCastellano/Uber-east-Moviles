45325c30059395046781d242a969b54b5844d92d
oriana castellano <orianacastella@gmail.com>
2021-08-01 12:10:56 -0400

    subida ori 17

--- src/app/sesion/sesion-chofer/sesion-chofer.page.ts
+++ src/app/sesion/sesion-chofer/sesion-chofer.page.ts
@@ -6,7 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sesion-chofer.page.scss'],
 })
 export class SesionChoferPage implements OnInit {
+  lista: string[];
 
+  delivery: any=[
+    {
+      cliente: "Oriana",
+      idfactura: 1,
+    },{
+      cliente: "Isabel",
+      idfactura: 1,
+    },{
+      cliente: "Castellano",
+      idfactura: 1,
+    },{
+      cliente: "Vivas",
+      idfactura: 1,
+    },{
+      cliente: "Jeliana",
+      idfactura: 1,
+    },{
+      cliente: "Parra",
+      idfactura: 1,
+    },
+
+  ]
   constructor() { }
 
   ngOnInit() {
